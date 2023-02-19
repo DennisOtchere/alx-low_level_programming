aa7937e94fe0684d8093beb1614ed141c8c894f7
Dennis Otchere <sekyeredennis3@gmail.com>
2023-02-19 15:38:43 -0800

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (dig1 =  0; dig1 < 9; dig1++)
 	{
-		for (dig2 = 0; dig2 < 10; dig2++)
+		for (dig2 = dig1 + 1; dig2 < 10; dig2++)
 		{
 			if ((dig1 == 0 && dig2 == 0) || (dig1 == 1 && dig2 == 0))
 			{
