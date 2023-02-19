ee084cb10cdc41f21c61ca66c1e0eb55cb6f97e9
Dennis Otchere <sekyeredennis3@gmail.com>
2023-02-19 13:16:35 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,20 +6,16 @@
 */
 int main(void)
 {
-	int dig1, dig2;
+	int dig1;
 
 	for (dig1 = 0; dig1 < 10; dig1++)
 	{
-		for (dig2 = 0; dig2 < 10; dig2++)
-		{
-			putchar(dig1 + '0');
-			putchar(dig2 + '0');
-			putchar(',');
-		}
+		putchar(dig1 + '0');
+		putchar(',');
+		putchar(' ');
 	}
 
 	putchar('\n');
 	return (0);
-
 
 }
