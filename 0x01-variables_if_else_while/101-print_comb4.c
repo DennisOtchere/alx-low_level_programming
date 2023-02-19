55226c0419623a2c7eca4e44c2dae8985494f4fe
Dennis Otchere <sekyeredennis3@gmail.com>
2023-02-19 15:57:25 -0800

    101-print_comb4.c modified

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,9 +10,9 @@ int main(void)
 
 	for (dig1 = 0; dig1 < 8; dig1++)
 	{
-		for (dig2 = dig1; dig2 < 9; dig2++)
+		for (dig2 = dig1 + 1; dig2 < 9; dig2++)
 		{
-			for (dig3 = dig2; dig3 < 10; dig3++)
+			for (dig3 = dig2 + 1; dig3 < 10; dig3++)
 			{
 				putchar(dig1 + '0');
 				putchar(dig2 + '0');
