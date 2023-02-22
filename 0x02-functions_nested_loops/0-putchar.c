2e3a5a09de1888fccc7304468b41dcb1e0cf4839
Dennis Otchere <sekyeredennis3@gmail.com>
2023-02-21 17:20:59 -0800

    added omited Readme, 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * main - entry point
+  * Description: a program that prints _putchar, followed by a new line
+  * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#This Readme file describes the projects under the 0x02-functions_nested_loops directory
+This file will be modified properly in future, forgive the scantiness at the moment
