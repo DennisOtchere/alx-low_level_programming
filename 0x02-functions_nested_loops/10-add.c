e416f5eb6a4f087776bb00e13002bd154d2a56e6
Dennis Otchere <sekyeredennis3@gmail.com>
2023-02-22 08:43:01 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * add - adds two integer arguments and returns result
+  * @num1: first integer parameter
+  * @num2: second integer parameter
+  * Return: sum returns the addition  of two integer values (Success)
+*/
+
+int add(int num1, int num2)
+{
+	int sum = num1 + num2;
+
+	return (sum);
+
+}
