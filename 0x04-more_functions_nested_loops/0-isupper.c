310fcf13f30f50282efa748c4799f46f28c09615
Dennis Otchere <sekyeredennis3@gmail.com>
2023-02-24 21:30:37 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * _isupper - checks for uppercase character
+  * @c: Parameter argument  text
+  * Return:  0 (Success).
+*/
+int _isupper(int c)
+{
+
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
