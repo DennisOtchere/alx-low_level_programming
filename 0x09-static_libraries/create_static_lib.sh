91129c8845c8c518aa9a6a97925a62f18b83e218
Dennis Otchere <sekyeredennis3@gmail.com>
2023-03-14 14:12:05 -0700

    modified create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# list all .c files and store in c_files 
-c_files = $(ls *.c)
 
-# generate object files from .c files stored in $c_files
-for file in $c_files
-do
 gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c
-done
 
-# create static library liball.a
 ar -rc liball.a *.o
