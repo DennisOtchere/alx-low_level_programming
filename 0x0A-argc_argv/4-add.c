f357270d6597bb12792271e87187c4697521d733
Dennis Otchere <sekyeredennis3@gmail.com>
2023-03-18 13:00:34 -0700

    corrected betty errors for 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-
+ * main - entry point of program
+ * @argc: argument count
+ * @argv: string argument at exec
+ *
+ * Return: 0 (success)
 */
 int main(int argc, __attribute__((unused))char *argv[])
 {
 	int sum, index;
 
 	sum = 0;
-	
+
 	if (argc == 1)
 		printf("0\n");
 	for (index = 1; index < argc; index++)
