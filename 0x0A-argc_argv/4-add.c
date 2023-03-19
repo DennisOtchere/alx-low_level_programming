72047257aa62cab2f7b3f08752a7f29cc1f46522
Dennis Otchere <sekyeredennis3@gmail.com>
2023-03-19 11:36:47 -0700

    removed atoi to check integer, created _isdigit function to do the check

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int _isdigit(char *str);
+
 /**
  * main - entry point of program
  * @argc: argument count
@@ -7,6 +10,8 @@
  *
  * Return: 0 (success)
 */
+
+
 int main(int argc, __attribute__((unused))char *argv[])
 {
 	int sum, index;
@@ -17,7 +22,7 @@ int main(int argc, __attribute__((unused))char *argv[])
 		printf("0\n");
 	for (index = 1; index < argc; index++)
 	{
-		if (!atoi(argv[index]))
+		if (_isdigit(argv[index]) == 1)
 		{
 			printf("Error\n");
 			return (1);
@@ -26,5 +31,22 @@ int main(int argc, __attribute__((unused))char *argv[])
 	}
 	if (sum > 0)
 		printf("%d\n", sum);
+	return (0);
+}
+
+/**
+ * _isdigit - checks if the parameter passed is an integer
+ * @str: input parameter/argument
+ *
+ * Return: 0 if is an integer, and 1 if its not
+*/
+int _isdigit(char *str)
+{
+	while (*str)
+	{
+		if (*str < '0' || *str > '9')
+			return (1);
+		str++;
+	}
 	return (0);
 }
