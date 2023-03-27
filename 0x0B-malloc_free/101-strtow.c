298e41d37a48ee7b67ee6a64e02cfa7552823e3e
Dennis Otchere <sekyeredennis3@gmail.com>
2023-03-27 22:03:55 +0000

    modified 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,52 +1,48 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include "main.h"
 /**
  * strtow - function that splits a string into words.
- * @str: string to be split into words
+ * @str: string to be splitted into words
  * Return: Pointer to the array of words of the string on success
  */
 char **strtow(char *str)
 {
+	int i, j, k, num_of_words = 0;
+	char **words;
+
 	if (str == NULL || *str == '\0')
 		return (NULL);
-	int num_of_words = 0;
-
-	for (int i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (isalpha(str[i]) && (i == 0 || !isalpha(str[i - 1])))
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 			num_of_words++;
 	}
-	char **words = malloc(sizeof(char *) * (num_of_words + 1));
-
+	words = malloc(sizeof(char *) * (num_of_words + 1));
 	if (words == NULL)
 		return (NULL);
-	int word_index = 0;
-
-	for (int i = 0; str[i] != '\0'; i++)
+	k = 0;
+	for (i = 0; i < num_of_words; i++)
 	{
-		if (isalpha(str[i]) && (i == 0 || !isalpha(str[i - 1])))
+		while (*str == ' ')
+			str++;
+		j = 0;
+		while (str[j] != ' ' && str[j] != '\0')
+			j++;
+
+		words[i] = malloc(sizeof(char) * (j + 1));
+		if (words[i] == NULL)
 		{
-			int word_len = 0;
-
-			while (isalpha(str[i + word_len]))
-				word_len++;
-
-			words[word_index] = malloc(sizeof(char) * (word_len + 1));
-			if (words[word_index] == NULL)
-			{
-				for (int j = 0; j < word_index; j++)
-					free(words[j]);
-				free(words);
-				return (NULL);
-			}
-			strncpy(words[word_index], &str[i], word_len);
-			words[word_index][word_len] = '\0';
-			word_index++;
-			i += word_len - 1;
+			for (k = 0; k < i; k++)
+				free(words[k]);
+			free(words);
+			return (NULL);
 		}
+		strncpy(words[i], str, j);
+		words[i][j] = '\0';
+		str += j;
 	}
-	words[word_index] = NULL;
+	words[num_of_words] = NULL;
 	return (words);
 }
-
