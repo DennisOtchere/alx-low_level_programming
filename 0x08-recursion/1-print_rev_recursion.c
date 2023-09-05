2d750c933e5d7d3bd12bbc217b050698caf8c310
Dennis <sekyeredennis3@gmail.com>
2023-09-05 15:34:49 +0100

    Add 1-print_rev_recursion.c and update README

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/* _print_rev_recursion - function that recursively prints a string in reverse
+ * @*s: string parameter (pointer to a character
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+	if(*s == '\0')
+	{
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,4 +1,4 @@
 # Project 0x08. C - Recursion
 
 1. void _puts_recursion(char *s): a function that prints a string, followed by a new line.
-2. 
+2. void _print_rev_recursion(char *s): a function that prints a string in reverse
